9d75bef8093a328cc1260b4309a477f3f6d0f6c8
ziangtang <tang.zia@northeastern.edu>
2024-11-04 14:50:56 -0800

    Updated selected files

--- app/src/main/java/com/example/myapplication/AboutMeActivity.java
+++ app/src/main/java/com/example/myapplication/AboutMeActivity.java
@@ -9,16 +9,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AboutMeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about_me);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_about_me);  // Ensure this matches your layout file name
     }
 }
